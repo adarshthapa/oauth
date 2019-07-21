039502a6325eec6e3d7079d1a61fc264db768cb2
Adarsh Thapa <realadarshthapa@gmail.com>
2019-07-21 06:43:24 +0545

    Connected to MongoDB Cloud through Mongoose

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const authRoutes = require('./routes/auth');
 const passportSetup = require('./config/passport');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
 const app = express();
 
+// view engine
 app.set('view engine', 'ejs');
 
+// connect to MongoDB Cloud
+mongoose.connect(keys.mongoDB.dbURI, () => {
+    console.log('Connected to the MongoDB Cloud');
+})
+
+// Middlewares
 app.use('/auth', authRoutes);
 
+// home route
 app.get('/', (req, res) => {
     res.render('home');
 })
